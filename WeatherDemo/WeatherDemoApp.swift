7a56dab4e8b435e24085616b7853229ba3cd7058
Alberto Orrantia <ricco.albert@gmail.com>
2023-08-29 14:33:37 -0700

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Alberto Orrantia on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
